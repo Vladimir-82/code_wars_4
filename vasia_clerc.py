c4742d0bebcc6223cbaaa0ff9b1dccc0de4d3b76
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-08-16 16:55:06 +0300

    initial

--- vasia_clerc.py
+++ vasia_clerc.py
@@ -1,10 +1,27 @@
 def tickets(people):
-    "Vasya - Clerk"
+    '''Vasya - Clerk'''
     cassa = []
+    if 25 not in people:
+        return 'NO'
     for i in people:
         if i == 25:
             cassa.append(i)
         else:
+            stop = 1
+            while stop <= len(cassa):
+                change = cassa[:stop]
+                if sum(change) == i - 25:
+                    cassa = cassa[stop:]
+                    cassa.append(i)
+                    change = []
+                    break
+                else:
+                    stop += 1
+                    if stop > len(cassa):
+                        return 'NO'
+
+    return 'YES'
 
 
-print(tickets([25, 25, 50]))
+
+print(tickets([25, 25, 25, 25, 50, 100, 50]))
