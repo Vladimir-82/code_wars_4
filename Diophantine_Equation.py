bdd0f106ace9773109d31f184af86595dad7ee03
vladimir <grun_gespenst@tut.by>
2021-08-20 19:53:03 +0300

    num hz

--- Diophantine_Equation.py
+++ Diophantine_Equation.py
@@ -6,13 +6,12 @@ def sol_equa(n):
     out = []
     for y in range(n):
         x = (n + 4 * y ** 2)**0.5
-        x = int(x)
-        res = x ** 2 - 4 * y ** 2
-        if res == n:
-            out.append([x, y])
-        # if res > n:
-        #     break
-    return out
+        if str(x)[-2:] == '.0':
+            res = x ** 2 - 4 * y ** 2
+            if res == n:
+                out.append([int(x), y])
+
+    return sorted(out, reverse=True)
 
 
 start = time.time()
