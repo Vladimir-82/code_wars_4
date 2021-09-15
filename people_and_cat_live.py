c4645b2864f243ff7b0efede005144dd7ae1d79d
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-09-15 16:46:45 +0300

    crazy house

---
+++ people_and_cat_live.py
@@ -0,0 +1,52 @@
+class Сreation:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 10
+        self.house = None
+
+
+
+
+class Man(Сreation):
+
+    def __init__(self, name):
+        super().__init__(name=name)
+        self.happiness = 10
+
+    def __str__(self):
+        return 'Житель - {}, Сытость - {}, Счастье - {}'.format(self.name, self.fullness, self.happiness)
+
+    def play_dota(self):
+        print('{} играет в доту'.format(self.name))
+        self.fullness -= 10
+
+    def go_into_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        print('{} въехал в дом'.format(self.name))
+
+
+
+class Cat(Сreation):
+
+    def pear_wallpaper(self):
+        print('{} дерёт обои'.format(self.name))
+        self.house.rubbish += 10
+
+    def go_into_house(self, house):
+        self.house = house
+        print('{} въехал в дом'.format(self.name))
+
+
+class House:
+    def __init__(self):
+        self.food = 30
+        self.cats_food = 30
+        self.money = 50
+        self.rubbish = 0
+
+    def __str__(self):
+        return 'В доме осталось еды - {}, кошачей еды - {}, денег - {}, мусора - {}'.format(
+            self.food, self.cats_food, self.money, self.rubbish)
+
--- probe.py
+++ probe.py
@@ -1,52 +1,80 @@
+import random
 
-# import itertools
-# def sum_arrangements(num):
-#     '''
-#     Sum of all numbers with the same digits
-#
-#     '''
-#     tpl = itertools.permutations(str(num), len(str(num)))
-#     s = 0
-#     for i in tpl:
-#         s += int(''.join(i))
-#
-#     return s
-#
-#
-#
-#
-# print(sum_arrangements(1185))
+SUBJ = ['condoms', 'chocolate', 'water', 'whisky', 'phone', 'pm3', 'dildo', 'ass plagin']
 
-def sum_arrangements(num):
-    '''
-    Sum of all numbers with the same digits
+class BackPack:
 
-    '''
-    k = 0
-    for i in str(num):
-        k += int(i)
+    def __init__(self, name, volume):
+        self.name = name
+        self.volume = volume
+        self.content = []
+
+    def add(self, subject):
+        if subject not in self.content:
+            print('В рюкзак ' + self.name + ' положили:', subject)
+            self.content.append(subject)
+
+    def inspect(self):
+        print('В рюкзаке: ', self.content)
+
+    def __str__(self):
+        return 'В рюкзаке ' + self.name + ': ' + ', '.join(self.content)
+
+    def __bool__(self):
+        return self.content != []
+
+    def __len__(self):
+        return len(self.content)
 
 
-    res = ''
-    size = len(str(num))
+    def __eq__(self, other):
+        return self.content == other.content
 
-    k = k * (size - 1)
-    ost = k
 
-    while size:
-        num = k + ost
-        num = str(num)
-        if size == 1:
-            res = num + res
-            break
-        res = num[-1] + res
-        ost = int(num[0])
-        size -= 1
-
-    return int(res) - k
+    def __add__(self, other):
+        new_backpack = BackPack('Новый', 2)
+        new_backpack.content.extend(self.content)
+        new_backpack.content.extend(other.content)
+        return new_backpack
 
 
 
+my_backpack = BackPack('Вова', 4)
+
+wives_backpack = BackPack('Оля', 3)
 
 
-print(sum_arrangements(1185))
+
+while True:
+
+    if len(my_backpack.content) < my_backpack.volume:
+        my_backpack.add(random.choice(SUBJ))
+
+
+    if len(wives_backpack.content) < wives_backpack.volume:
+       wives_backpack.add(random.choice(SUBJ))
+
+
+    if len(my_backpack.content) == my_backpack.volume and \
+            len(wives_backpack.content) == wives_backpack.volume:
+                print('Рюкзаки заполнены')
+                break
+
+print(my_backpack)
+print(wives_backpack)
+
+if my_backpack == wives_backpack:
+    print('we are so once')
+else:
+    print('we are difference')
+
+
+new = my_backpack + wives_backpack
+print(new)
+
+
+
+
+
+
+
