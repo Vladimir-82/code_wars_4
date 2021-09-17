e4068aa9921ff75902ba2367a199c33163d96b13
vladimir <grun_gespenst@tut.by>
2021-09-17 21:57:22 +0300

    tower complite

--- Build_Tower.py
+++ Build_Tower.py
@@ -1,8 +1,12 @@
 def tower_builder(n):
     'Build Tower'
-    for i in range(1, n + 1):
-        var = 2 * (n - 1)
-        print(var // 2 - ())
 
+    res = []
+    size = 2 * n - 1
+    for i in range(1, n + 1):
+
+        var = 2 * i - 1
+        res.append(((size - var) // 2) * ' ' + var * '*' + ((size - var) // 2) * ' ')
+    return res
 
 print(tower_builder(6))
